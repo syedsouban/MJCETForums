0bd5e95a0b689764e101df2a432830e2d5342f82
Ajaaz <mdajaaz1999@gmail.com>
2017-09-29 10:31:40 +0530

    added fonts and also the custom view for feed, also added buttons in resources done with feedclass and also it's adapter, buutons are such a pain in the butt they are still .pending... added icons for navbar :)

--- app/src/main/java/com/syedsauban/mjforums/demo.java
+++ app/src/main/java/com/syedsauban/mjforums/demo.java
@@ -1,26 +1,22 @@
 package com.syedsauban.mjforums;
 
-import android.app.Fragment;
+/**
+ * Created by Ajaaz on 9/29/2017.
+ */
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-/**
- * Created by Syed on 25-08-2017.
- */
+public class demo extends AppCompatActivity {
 
-public class Newest extends android.support.v4.app.Fragment {
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        final View rootView = inflater.inflate(R.layout.newest, container, false);
-        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerviewinfeed);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newest);
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerviewinfeed);
         ArrayList<FeedClass> a1 = new ArrayList<FeedClass>();
         a1.add(new FeedClass("who is hod", "Ajaz", "Souban", "Our hod is Moiz Qayser", "Intern at Chronology", R.drawable.answer_profile));
         a1.add(new FeedClass("who is hod", "Ajaz", "Souban", "Our hod is Moiz Qayser", "Intern at Chronology", R.drawable.answer_profile));
@@ -29,11 +25,9 @@ public class Newest extends android.support.v4.app.Fragment {
         a1.add(new FeedClass("who is hod", "Ajaz", "Souban", "Our hod is Moiz Qayser", "Intern at Chronology", R.drawable.answer_profile));
         FeedClassAdapter adapter = new FeedClassAdapter(a1);
         recyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-        return rootView;
-
 
     }
 }
